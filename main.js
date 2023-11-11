9832ccdd7fff6ad734b124f373aea984b00b9dd0
lolitaklim <lolita.klim@yandex.ru>
2023-11-11 04:50:00 +0300

    class name

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // перенос слов с дефисами на новою строку в названиях товаров
 function preventHyphenation() {
-    const elements = document.getElementsByClassName("in-stock-item-content-description-text");
+    const elements = document.getElementsByClassName("goods-info__title");
     const elementsArray = Array.from(elements);
     elementsArray.forEach(element => {
         element.innerHTML = element.innerHTML.replace(/(\w+)-(\w+)/g, "<span class=\"nowrap\">$1-$2</span>");
